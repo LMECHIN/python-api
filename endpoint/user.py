3a0e262df164867f2c321caf44983b421b1359fb
LMECHIN <lmechin@savprogroupe.fr>
2024-01-16 18:29:38 +0100

    [EDIT] update error -> auth_header

--- endpoint/user.py
+++ endpoint/user.py
@@ -7,10 +7,10 @@ from token_api import authenticated_users
 def requires_auth(func):
     def wrapper(*args, **kwargs):
         auth_header = request.headers.get("Authorization")
-        if not auth_header or "Bearer" not in auth_header:
+        if not auth_header or not auth_header.startswith("Bearer "):
             return jsonify({"error": "Unauthorized"}), 401
 
-        token = auth_header.split(" ")[1]
+        token = auth_header.split(" ")[1] if len(auth_header.split(" ")) > 1 else None
         user = authenticated_users.get(token)
 
         if not user:
