ec17c1d7006aa66fe0c142c22aa9b4344caa5c92
LMECHIN <lucas.mechin@epitech.eu>
2024-02-19 18:19:03 +0100

    [EDIT] change endpoint edit task -> fix

--- src/endpoint/task/edit_task.py
+++ src/endpoint/task/edit_task.py
@@ -15,21 +15,25 @@ def edit_task():
 
     if request.headers["Content-Type"] == "application/json":
         data = request.json
+        task_id = data.get("task_id")
         new_title = data.get("new_title")
         new_description = data.get("new_description")
         new_status = data.get("new_status")
         new_priority = data.get("new_priority")
     else:
         data = request.form
+        task_id = data.get("task_id")
         new_title = data.get("new_title")
         new_description = data.get("new_description")
         new_status = data.get("new_status")
         new_priority = data.get("new_priority")
 
+    cursor = None
+
     try:
         cursor = db.cursor()
 
-        cursor.execute("SELECT * FROM tasks WHERE user_id = %s;", (user_id,))
+        cursor.execute("SELECT * FROM tasks WHERE user_id = %s AND task_id = %s;", (user_id, task_id))
         task_data = cursor.fetchone()
         if not task_data:
             response = {"error": "Task not found"}
@@ -41,37 +45,37 @@ def edit_task():
             task_data[5],
             task_data[6],
         )
-
+        # cursor.fetchall()
+        print(task_id)
         print(title)
         print(description)
         print(status)
         print(priority)
+        print(task_id)
 
         if new_title is not None:
-            cursor.execute(
-                "UPDATE tasks SET title = %s WHERE title = %s AND user_id = %s;",
-                (new_title, title, user_id),
-            )
+            query = "UPDATE tasks SET title = %s WHERE title = %s AND user_id = %s AND task_id = %s;"
+            cursor.execute(query, (new_title, title, user_id, task_id))
             title = new_title
 
         if new_description is not None:
             cursor.execute(
-                "UPDATE tasks SET description = %s WHERE description = %s AND user_id = %s;",
-                (new_description, description, user_id),
+                "UPDATE tasks SET description = %s WHERE description = %s AND user_id = %s AND task_id = %s;",
+                (new_description, description, user_id, task_id),
             )
             description = new_description
 
         if new_status is not None:
             cursor.execute(
-                "UPDATE tasks SET status = %s WHERE status = %s AND user_id = %s;",
-                (new_status, status, user_id),
+                "UPDATE tasks SET status = %s WHERE status = %s AND user_id = %s AND task_id = %s;",
+                (new_status, status, user_id, task_id),
             )
             status = new_status
 
         if new_priority is not None:
             cursor.execute(
-                "UPDATE tasks SET priority = %s WHERE priority = %s AND user_id = %s;",
-                (new_priority, priority, user_id),
+                "UPDATE tasks SET priority = %s WHERE priority = %s AND user_id = %s AND task_id = %s;",
+                (new_priority, priority, user_id, task_id),
             )
             priority = new_priority
 
